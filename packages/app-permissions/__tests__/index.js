790e77a8bb487270dcc818fc3efd605f8efdc2b3
Duane Johnson <duane.johnson@gmail.com>
2018-06-26 13:42:07 -0600

    Tests for app-permissions (#22)
    * Add some tests to app-permissions package

---
+++ packages/app-permissions/__tests__/index.js
@@ -0,0 +1,59 @@
+import { EMPTY_DEFINITIONS, mergeGrantsToDetails } from '../lib'
+
+describe('EMPTY_DEFINITIONS', () => {
+  it('has HTTPS_REQUEST', () => {
+    expect(EMPTY_DEFINITIONS).toHaveProperty('HTTPS_REQUEST')
+  })
+})
+
+describe('mergeGrantsToDetails', () => {
+  it('merges', () => {
+    const appGrants = {
+      WEB3_SEND: false,
+      HTTPS_REQUEST: {
+        granted: ['https://google.com', 'https://twitter.com'],
+        denied: [],
+      },
+    }
+    const userGrants = {
+      LOCATION_GET: true,
+      HTTPS_REQUEST: {
+        granted: ['https://mainframe.com'],
+        denied: ['https://facebook.com'],
+      },
+    }
+
+    const details = {
+      app: appGrants,
+      user: userGrants,
+      session: {
+        HTTPS_REQUEST: {
+          granted: [
+            'https://google.com',
+            'https://twitter.com',
+            'https://mainframe.com',
+          ],
+          denied: ['https://facebook.com'],
+        },
+        LOCATION_GET: true,
+        WEB3_SEND: false,
+      },
+    }
+
+    expect(mergeGrantsToDetails(appGrants, userGrants)).toEqual(details)
+  })
+
+  it('session HTTPS_REQUEST grants are concatenated', () => {
+    let g1 = ['https://google.com', 'https://twitter.com']
+    let g2 = ['https://mainframe.com']
+    let result = mergeGrantsToDetails(
+      { HTTPS_REQUEST: { granted: g1, denied: [] } },
+      { HTTPS_REQUEST: { granted: g2, denied: [] } },
+    )
+    expect(result.session.HTTPS_REQUEST.granted).toEqual([
+      'https://google.com',
+      'https://twitter.com',
+      'https://mainframe.com',
+    ])
+  })
+})
