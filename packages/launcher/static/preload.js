29c82946af0d18e029cb97ac5085c9c39794db36
Doug L <douglaswleonard+git@gmail.com>
2018-12-14 14:17:02 -0700

    update preload.js to match patterns seen in metamask and mist

--- packages/launcher/static/preload.js
+++ packages/launcher/static/preload.js
@@ -1,12 +1,24 @@
+/**
+ Sets the ethereum provider, as well as "web3" for backwards compatibility.
+ **/
 const electronRPC = require('@mainframe/rpc-electron').default
 const { getWeb3Provider } = require('@mainframe/web3-provider')
-
 const rpc = electronRPC('rpc-app-sandboxed')
-const web3Provider = getWeb3Provider(rpc)
+const Web3 = require('web3')
+const BigNumber = require('bignumber.js')
 
 window.mainframe = { rpc }
-window['web3'] = { currentProvider: web3Provider } // Existing standard
-window.ethereum = web3Provider // TODO: Provider needs updating to support this new standard https://eips.ethereum.org/EIPS/eip-1102
 
-// set a high max listener count to avoid unnecessary warnings
+const web3Provider = getWeb3Provider(rpc)
 web3Provider.setMaxListeners(100)
+window.ethereum = web3Provider
+
+// Existing standard
+// TODO: Provider needs updating to support this new standard https://eips.ethereum.org/EIPS/eip-1102
+window.BigNumber = BigNumber;
+window.web3 = {
+  currentProvider: web3Provider,
+}
+
+// for now still add this too: WILL BE REMOVED when mist migrates to web3 1.0 & removes it from their preload script
+window.web3 = new Web3(web3Provider)
