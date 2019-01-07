1643b0bad1e5ea1309dd6c4c51c78015fb58807a
profiq <profiq>
2019-01-07 14:56:09 +0100

    reorganized launcher tests

---
+++ packages/launcher/test/utils.js
@@ -0,0 +1,42 @@
+const timeouts = {
+  unlockVault: 10000,
+  input: 2000,
+  launch: 5000,
+}
+
+const clearConsole = app => {
+  app.client.log('browser') //
+}
+
+const checkConsole = (app, logType = 'browser', level = 'SEVERE') => {
+  return app.client.log(logType).then(logs => {
+    return logs.value.filter(log => {
+      return log.level == level
+    })
+  })
+}
+
+const unlockVault = async function(app,password) {
+  try {
+    await app.client
+      .element('[data-testid="vault-manager-unlock-input"]')
+      .setValue(password)
+    await app.client
+      .element('[data-testid="vault-manager-unlock-button"]')
+      .click()
+    await app.client.waitForExist(
+      '[data-testid="launcher-view"]',
+      timeouts.unlockVault,
+    )
+    return { unlocked: true, error: '' }
+  } catch (err) {
+    return { unlocked: false, error: err }
+  }
+}
+
+module.exports = {
+  timeouts: timeouts,
+  checkConsole: checkConsole,
+  clearConsole: clearConsole,
+  unlockVault: unlockVault,
+}
