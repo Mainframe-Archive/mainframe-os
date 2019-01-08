c4a129f08f5d5e84c827448b35b607d309d54d68
Doug <douglaswleonard+git@gmail.com>
2019-01-08 15:57:53 +0000

    add contactsAPIs

--- packages/sdk/src/index.js
+++ packages/sdk/src/index.js
@@ -6,11 +6,13 @@ import type StreamRPC from '@mainframe/rpc-stream'
 
 import BlockchainAPIs from './apis/Blockchain'
 import PssAPIs from './apis/Pss'
+import ContactsAPIs from "./apis/Contacts";
 
 export default class MainframeSDK {
   _rpc: StreamRPC
   blockchain: BlockchainAPIs
   pss: PssAPIs
+  contacts: ContactsAPIs
 
   constructor() {
     if (window.mainframe) {
@@ -21,6 +23,7 @@ export default class MainframeSDK {
 
     this.blockchain = new BlockchainAPIs(this._rpc)
     this.pss = new PssAPIs(this._rpc)
+    this.contacts = new ContactsAPIs(this._rpc)
   }
 
   apiVersion = () => {
