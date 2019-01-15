cb784b610d2677d4f656d6aae00a94ffd5cdd4c2
Doug <douglaswleonard+git@gmail.com>
2019-01-15 11:37:44 +0000

    remove users

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,18 +8,6 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
@@ -43,9 +31,6 @@ const siteConfig = {
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
-
-  // If you have users set above, you add it here:
-  users,
 
   /* path to images for header/footer */
   headerIcon: 'img/docusaurus.svg',
