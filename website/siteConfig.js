c46049703702ef82a3e8dfc90455535c0e6a289e
Doug <douglaswleonard+git@gmail.com>
2019-01-15 11:54:45 +0000

    pruning

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,17 +9,17 @@
 // site configuration options.
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'Mainframe Developer Portal', // Title for your website.
+  tagline: 'Get started using Mainframe.',
+  url: 'https://mainframe.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'dev-portal',
+  organizationName: 'Mainframe',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -29,7 +29,6 @@ const siteConfig = {
     {doc: 'doc1', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
   ],
 
   /* path to images for header/footer */
@@ -58,7 +57,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Mainframe`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
