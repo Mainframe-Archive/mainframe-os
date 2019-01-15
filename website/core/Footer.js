958a70a85ce4a3352781fe2ef0a2c374b8b2c8b2
Doug <douglaswleonard+git@gmail.com>
2019-01-15 11:39:37 +0000

    remove more users

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -49,9 +49,6 @@ class Footer extends React.Component {
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
             <a
               href="http://stackoverflow.com/questions/tagged/"
               target="_blank"
