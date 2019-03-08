32b05420b65e80bd8eaecdf45ace84b45832bd84
Miloš Mošić <milos@mainframe.com>
2019-03-08 19:16:07 +0000

    Remove obsolete crypto code

--- packages/launcher/src/main/storage.js
+++ packages/launcher/src/main/storage.js
@@ -1,55 +1,8 @@
 // @flow
 
-import crypto from 'crypto'
-import { Transform } from 'stream'
 import * as mime from 'mime'
-import type { AppContext } from './contexts'
 import { createDecryptStream } from '@mainframe/utils-crypto'
-
-const INITIALIZATION_VECTOR_SIZE = 16
-
-export class PrependInitializationVector extends Transform {
-  constructor(iv) {
-    super()
-    this._added = false
-    this._iv = iv
-  }
-
-  _transform(chunk, encoding, callback) {
-    if (!this._added) {
-      this._added = true
-      this.push(this._iv)
-    }
-    this.push(chunk)
-    callback()
-  }
-}
-
-export class Decrypt extends Transform {
-  constructor(cipherKey) {
-    super()
-    this.cipherKey = cipherKey
-  }
-
-  _transform(chunk, encoding, callback) {
-    if (!this._decode) {
-      const iv = chunk.slice(0, INITIALIZATION_VECTOR_SIZE)
-
-      this._decode = crypto.createDecipheriv('aes256', this.cipherKey, iv)
-      this._decode.on('data', c => {
-        this.push(c)
-      })
-      this._decode.on('end', () => {
-        this.emit('end')
-      })
-
-      this._decode.write(chunk.slice(INITIALIZATION_VECTOR_SIZE, chunk.length))
-    } else {
-      this._decode.write(chunk)
-    }
-    callback()
-  }
-}
+import type { AppContext } from './contexts'
 
 export const registerStreamProtocol = (context: AppContext) => {
   context.sandbox.session.protocol.registerStreamProtocol(
@@ -67,7 +20,9 @@ export const registerStreamProtocol = (context: AppContext) => {
         const res = await context.bzz.download(
           `${context.storage.feedHash}/${filePath}`,
         )
-        const data = res.body.pipe(createDecryptStream(context.storage.encryptionKey))
+        const data = res.body.pipe(
+          createDecryptStream(context.storage.encryptionKey),
+        )
         callback({
           headers: {
             'content-type': contentType,
